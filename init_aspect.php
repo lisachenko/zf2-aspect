d4c51786de948618967e03ae270fa26a57c383f2
Alexander lisachenko <lisachenko.it@gmail.com>
2014-10-16 09:15:58 +0300

    Update dependencies and kernel initialization for easy start #1

--- init_aspect.php
+++ init_aspect.php
@@ -5,22 +5,15 @@ use Aspect\DemoAspectKernel;
 // Prevent an error about nesting level
 ini_set('xdebug.max_nesting_level', 500);
 
-include './vendor/lisachenko/go-aop-php/src/Go/Core/AspectKernel.php';
-include './aspect/Aspect/DemoAspectKernel.php';
+include __DIR__ . '/init_autoloader.php';
 
 // Initialize demo aspect container
 DemoAspectKernel::getInstance()->init(array(
-    'debug'         => false,
-    'appLoader'     => __DIR__ . '/init_autoloader.php',
+    'debug'         => true,
+    'appDir'        => __DIR__,
     'cacheDir'      => __DIR__ . '/data/cache/aspect',
     // Include paths for aspect weaving
-    'includePaths'  => array(),
-    // Configuration for autoload namespaces
-    'autoloadPaths' => array(
-        'Aspect'           => __DIR__ . '/aspect/',
-        'Go'               => __DIR__ . '/vendor/lisachenko/go-aop-php/src',
-        'TokenReflection'  => __DIR__ . '/vendor/andrewsville/php-token-reflection/',
-        'Doctrine\\Common' => __DIR__ . '/vendor/doctrine/common/lib/',
-        'Dissect'          => __DIR__ . '/vendor/jakubledl/dissect/src/',
-    ),
+    'excludePaths'  => array(
+        __DIR__ . '/data/cache/aspect',
+    )
 ));
